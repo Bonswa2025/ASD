8ed15e879c0970e7e7eb45382440fe6399aa5065
Bonswa2025 <weshley@gmail.com>
2025-11-10 13:31:47 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
 // src/main.jsx
-import './index.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StadslabSuiteHub from "./app.jsx";
+import App from "./app";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* Modules NIET direct importeren; we tonen alleen de hub.
-       Later kun je via `links` of window.STADSLAB_APPS registreren. */}
-    <StadslabSuiteHub
-      links={{
-        foodtruck: "",   // zet hier een URL als je stand-alone wil openen
-        backoffice: "",
-        staff: "",
-      }}
-    />
+    <App />
   </React.StrictMode>
 );
