140e902cbd4efd3ca8c4a7c6ad548d9ab5d81a96
Bonswa2025 <weshley@gmail.com>
2025-11-10 13:29:47 +0100

    Create medewerkers.jsx

---
+++ src/pages/medewerkers.jsx
@@ -0,0 +1,9 @@
+// src/pages/medewerkers.jsx
+export default function Medewerkers() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">medewerkers</h1>
+      <p className="text-gray-600">hier komt het medewerkerbeheer</p>
+    </div>
+  );
+}
