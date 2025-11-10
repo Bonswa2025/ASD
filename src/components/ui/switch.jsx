1dd94d17ffc89f2a7835d7bee37f4412eec36a08
Bonswa2025 <weshley@gmail.com>
2025-11-10 15:16:59 +0100

    Update switch.jsx

--- src/components/ui/switch.jsx
+++ src/components/ui/switch.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-export function Switch({checked, onCheckedChange}){
-  return <input type="checkbox" role="switch" checked={!!checked} onChange={e=>onCheckedChange?.(e.target.checked)} />;
-}
-import React from 'react';
-export function Switch({checked, onCheckedChange}){
+
+export function Switch({ checked, onCheckedChange, className = '', ...rest }) {
   return (
     <input
       type="checkbox"
       role="switch"
       checked={!!checked}
-      onChange={(e)=>onCheckedChange?.(e.target.checked)}
+      onChange={(e) => onCheckedChange?.(e.target.checked)}
+      className={className}
+      {...rest}
     />
   );
 }
