f61f3233e1e028c5bc601510e868ea8771efca95
Bonswa2025 <weshley@gmail.com>
2025-11-10 14:36:39 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from '@/layout/MainLayout'
 import Dashboard from '@/modules/dashboard'
 import Backoffice from '@/modules/backoffice'
-import Foodtruck from '@/modules/foodtruck'
+import Foodtruck from '@/modules/foodtruck'   // <-- deze import moet er zijn
 
 const router = createBrowserRouter([
   {
@@ -13,9 +13,9 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Dashboard /> },
       { path: 'backoffice', element: <Backoffice /> },
-      { path: 'foodtruck', element: <Foodtruck /> },
-    ]
-  }
+      { path: 'foodtruck', element: <Foodtruck /> }, // <-- route naar jouw module
+    ],
+  },
 ])
 
 createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
